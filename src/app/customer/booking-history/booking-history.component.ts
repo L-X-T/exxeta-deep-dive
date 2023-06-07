59668619f9df3382530da02ff136423fc5fa3f86
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-06-07 09:29:06 +0200

    Demonstrate viewContainerRef for lazyloaded components

--- src/app/customer/booking-history/booking-history.component.ts
+++ src/app/customer/booking-history/booking-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 
 import { Flight } from '../../flight-booking/flight';
 
@@ -10,15 +10,28 @@ import { Flight } from '../../flight-booking/flight';
 export class BookingHistoryComponent implements OnInit {
   flightSearchComponent: any;
 
+  @ViewChild('myComponentContainer', { read: ViewContainerRef }) myComponentContainer: any;
+
   flights: Flight[] = [
     { id: 1, from: 'Hamburg', to: 'Berlin', date: '2025-02-01T17:00+01:00' },
     { id: 2, from: 'Hamburg', to: 'Frankfurt', date: '2025-02-01T17:30+01:00' },
     { id: 3, from: 'Hamburg', to: 'Mallorca', date: '2025-02-01T17:45+01:00' }
   ];
 
+  constructor(private viewContainerRef: ViewContainerRef) {}
+
   async ngOnInit() {
     const esm = await import('../../flight-booking/flight-search/flight-search.component');
+    // alt 1 ngComponentOutlet
     this.flightSearchComponent = esm.FlightSearchComponent;
+
+    // alt 2 viewContainerRef
+    // const viewRef = this.viewContainerRef.createComponent(esm.FlightSearchComponent);
+    const viewRef = this.myComponentContainer.createComponent(esm.FlightSearchComponent);
+    const flightSearchInstance = viewRef.instance;
+    flightSearchInstance.from = 'Berlin';
+    flightSearchInstance.to = 'Zürich';
+    flightSearchInstance.search();
   }
 
   delete(): void {
